4921aac3a0b5df0afc2a67828c5b77dca63478a7
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2022-04-07 20:16:04 +0500

    add POST

--- src/main/java/com/example/demo/SimpleController.java
+++ src/main/java/com/example/demo/SimpleController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,11 @@ public class SimpleController {
 
     @GetMapping
     public String get(SimpleQuery query) {
+        return query.text();
+    }
+
+    @PostMapping
+    public String post(@RequestBody SimpleQuery query) {
         return query.text();
     }
 
